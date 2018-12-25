14935b41660387f0e6764658684903c783ec05a3
fun <fun@fundeMacBook-Pro.local>
2018-12-25 23:04:53 +0800

    first_api

--- flask_restful/fast-injection/first_api.py
+++ flask_restful/fast-injection/first_api.py
@@ -1,3 +1,15 @@
 # -*- coding:utf-8 -*-
 from flask import Flask
-import flask_restful
+from flask_restful import Resource,Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'Hello':'World'}
+
+api.add_resource(HelloWorld,'/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
